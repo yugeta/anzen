b634a0044a003bc2b2d4c60c0c87a2ae30e0189f
Yugeta.Koji <geta1972@gmail.com>
2025-01-16 14:19:11 +0900

    page: waste_liquid_pazzle

---
+++ public/waste_liquid_pazzle/js/main.js
@@ -0,0 +1,14 @@
+
+class Main{
+  constructor(){
+    console.log(1)
+  }
+}
+
+switch(document.readyState){
+  case "complete":
+  case "interactive":
+    new Main();break
+  default:
+    window.addEventListener("DOMContentLoaded", (()=>new Main()))
+}
